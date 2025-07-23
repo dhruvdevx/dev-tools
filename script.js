42709d16eebaa14bbea8adc7577476ab264ec4ef
Dhruv Upadhyay <dhruv@dhruvupadhyay.com>
2025-07-23 21:32:19 +0530

    Add files via upload
    fix: move files to root for GitHub Pages

---
+++ script.js
@@ -0,0 +1,8 @@
+function generatePassword() {
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$!";
+  let password = "";
+  for (let i = 0; i < 12; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  document.getElementById("password").value = password;
+}
