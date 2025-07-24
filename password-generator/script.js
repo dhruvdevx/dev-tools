4206c87136d3639629496f185a10bb3fbc4332e2
Dhruv Upadhyay <dhruv@dhruvupadhyay.com>
2025-07-24 23:17:25 +0530

    Delete password-generator/script.js

--- password-generator/script.js
+++
@@ -1,8 +0,0 @@
-function generatePassword() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-  let password = "";
-  for (let i = 0; i < 12; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  document.getElementById("password").value = password;
-}
